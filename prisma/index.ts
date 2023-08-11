1981388083df75fd05eb0b46cbca5678c8036856
Prajwal Neupane <prajwalneupane.981@gmail.com>
2023-08-11 15:32:06 +0545

    final commit

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,21 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import "../global.d.ts";
 
 let prisma: PrismaClient;
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient;
-    }
-  }
+
+if (process.env.NODE_ENV !== "production" && !global.prisma) {
+  global.prisma = new PrismaClient();
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+prisma = global.prisma || new PrismaClient();
 
 export default prisma;
